3082ab2bef4e7173da3f104eab7cb759cba32eb9
developerXDhanjit <developerdhanjit@gmail.com>
2024-05-23 19:21:51 +0530

    added vite project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -5,12 +6,20 @@ import './App.css'
 
 import Code from "./code"
 
+const myName = React.createElement(
+  'h2',
+  {href : "logo"},
+  'Hello ! Deveopers '
+)
+//pre-defined syntax
+
 function App() {
 
 
   return (
     <>
       <h3> Chai aur React | Dhanjit Nath</h3>
+      {myName} {/* evaluated expression => final outcome is only written here s */}
       <p> In this video I learned  </p>
       <ul>
         <li>How to create a basic react app using vite and other method</li>
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const someATag = (
+  <a href="https://www.google.com/">Visit Google</a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
